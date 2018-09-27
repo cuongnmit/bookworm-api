65afe2208018aa6c42d7fe91db0e42437649ed9c
Nguyen Manh Cuong <nguyenmanhcuong03@vccorp.vn>
2018-09-27 11:58:58 +0700

    End #05: finished signup

---
+++ mailer.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+
+const from = '"Bookworm" <info@bookworm.com>';
+function setup() {
+    return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+}
+
+export function sendConfirmationEmail(user) {
+    const tranport = setup();
+    const email = {
+        from,
+        to: user.email,
+        subject: 'Welcome to Bookworm',
+        html: `
+            <p>Welcome to Bookworm. Please, confirm your email.</p>
+
+            <a href="${user.generateConfirmationUrl()}">Click here</a>
+        `
+    };
+    tranport.sendMail(email);
+}
--- models/User.js
+++ models/User.js
@@ -7,7 +7,8 @@ const schema = new mongoose.Schema(
     {
         email: { type: String, required: true, lowercase: true, index: true, unique: true },
         passwordHash: { type: String },
-        confirmed: { type: Boolean, default: false }
+        confirmed: { type: Boolean, default: false },
+        confirmationToken: { type: String, default: '' }
     },
     { timestamps: true }
 );
@@ -18,7 +19,8 @@ schema.methods.isValidPassword = function isValidPassword(password) {
 
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign({
-        email: this.email
+        email: this.email,
+        confirmed: this.confirmed
     }, process.env.JWT_SECRET);
 }
 
@@ -32,6 +34,14 @@ schema.methods.toAuthJSON = function toAuthJSON() {
 
 schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcryptjs.hashSync(password, 10);
+}
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+}
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
 }
 
 schema.plugin(uniqueValidator, { message: 'This email is already taken'});
